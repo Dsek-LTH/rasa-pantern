add8ec62976db4da83ca39f26c9c6911e35d386a
Tomas-Ka <tomas.kamsater@gmail.com>
2025-12-27 03:02:36 +0100

    working base for role sync

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import final
+from typing import final, override
 
 
 @final
@@ -10,11 +10,20 @@ class RoleMapping:
         discord_role_id: int,
         guild_id: int,
         message_id: int | None = None,
+        channel_id: int | None = None,
     ) -> None:
         self.message_id = message_id
+        self.channel_id = channel_id
         self.role_id = role_id
         self.discord_role_id = discord_role_id
         self.guild_id = guild_id
+
+    @override
+    def __repr__(self) -> str:
+        return (
+            f"role: {self.role_id} linking to "
+            f"{self.discord_role_id} in guild {self.guild_id}"
+        )
 
 
 class CogSetting(Enum):
